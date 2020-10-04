e690e2997202b776e4440cf204f912abb9fbc40c
keirbailey <keirinvestment@gmail.com>
2020-10-04 15:45:28 +0100

    Updated config

--- src/test/java/finance/modelling/data/transform/transformincomestatements/TransformIncomeStatementsApplicationTests.java
+++ src/test/java/finance/modelling/data/transform/transformincomestatements/TransformIncomeStatementsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class transformincomestatementsApplicationTests {
+class TransformIncomeStatementsApplicationTests {
 
 	@Test
 	void contextLoads() {
